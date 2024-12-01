217cb4174510b7de7edce297f7ba0b31064ed73b
yee0328 <ppp91032862@gmail.com>
2024-12-01 20:44:27 +0800

    "feat: 查看維修及保養全部資料以及該細項資料"

--- Zhixun/js/preview_maintenance.js
+++ Zhixun/js/preview_maintenance.js
@@ -1,65 +1,68 @@
 $(document).ready(function () {
-  $(".slider").on("afterChange", function (event, slick, currentSlide) {
-    var currentImageSrc = $(".slider .slick-current ").attr("src");
-    $(".image-preview img").attr("src", currentImageSrc);
-  });
-
-  let data = callAPI();
-  fillInfo(data);
-});
-
-function callAPI() {
-  const data = {
-    title: "我是標題3",
-    uploader: "王小明",
-    uploadInfo: "我是說明3",
-    uploadDate: "2024-09-21",
-    imageFiles: [
-      { url: "../images/night_city.jpg", name: "Night City 1" },
-      { url: "../images/night_city.jpg", name: "Night City 2" },
-      { url: "../images/logo_black.png", name: "Night City 3" },
-      { url: "../images/night_city.jpg", name: "Night City 4" },
-    ],
-    pdfFiles: [
-      { url: "../files/demo.pdf", name: "demo.pdf" },
-      { url: "../files/demo.pdf", name: "demo.pdf" },
-    ],
-  };
-
-  return data;
-
+  const iconText = "機電"; // 設置預設值
+  const uploadtext = "維護及施工紀錄查詢";
+  var previewType = "維護及施工紀錄";
+  if (typeof BreadcrumbManager !== "undefined") {
+    console.log("1");
+    BreadcrumbManager.updateBreadcrumb(iconText, uploadtext, previewType);
+  }
+  var main_id = localStorage.getItem("mainid");
+  console.log(main_id);
   $.ajax({
-    url: "/api/getFiles",
+    url: `${window.API_CONFIG.baseUrl}/maintenanceDetail`,
     type: "GET",
-    success: function (response) {},
+    data: {
+      main_id: main_id,
+    },
+    success: function (response) {
+      console.log("Response data:", response);
+      data = response.data;
+      fillInfo(data);
+    },
     error: function (error) {
       console.log("Error fetching files:", error);
     },
   });
-}
+  $(".recordType").on("click", function () {
+    window.location.href = "records_maintenance.html";
+  });
+});
 
 function fillInfo(data) {
+  console.log(data);
+  var date = data[0].date.substring(0, 10);
   // 基本資料
-  $(".upload-title").text(data.title);
-  $(".upload-info").text(data.uploadInfo);
-  $(".uploader").text(data.uploader);
-  $(".upload-date").text(data.uploadDate);
-
-  // 圖片
+  $(".upload-title").text(data[0].main_class);
+  $(".upload-info").text(data[0].main_Detail);
+  $(".postion-info").text(data[0].main_postion);
+  $(".uploader").text(data[0].user);
+  $(".upload-date").text(date);
+  var path = "../../file/maintenance/";
+  $(".slider").on("afterChange", function (event, slick, currentSlide) {
+    var currentImageSrc = $(".slider .slick-current ").attr("src");
+    $(".image-preview img").attr("src", currentImageSrc);
+  });
   const sliderContainer = $(".slider");
   sliderContainer.empty();
-
-  data.imageFiles.forEach(function (imageFile) {
-    const imageElement = `<img src="${imageFile.url}" alt="${imageFile.name}" class="slider-image">`;
-    sliderContainer.append(imageElement);
+  const pdfListContainer = $(".uploaded-file");
+  pdfListContainer.empty();
+  data[0].file_name.forEach(function (File) {
+    var floder = File.split(".")[1];
+    if (floder == "pdf") {
+      const pdfElement = `<li><a href="${path}${floder}/${File}" target="_blank" download>${File} <i class="fa-solid fa-external-link-alt"></i></a></li>`;
+      pdfListContainer.append(pdfElement);
+    } else {
+      floder = "photo";
+      const imageElement = `<img src="${path}${floder}/${File}" alt="${File}" class="slider-image">`;
+      sliderContainer.append(imageElement);
+    }
   });
-
   $(".slider").slick({
     dots: true,
     infinite: false,
     centerMode: true,
     speed: 300,
-    slidesToShow: 3,
+    slidesToShow: 1,
     slidesToScroll: 1,
     prevArrow:
       '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
@@ -69,19 +72,17 @@ function fillInfo(data) {
 
   var initialImageSrc = $(".slider .slick-current").attr("src");
   $(".image-preview img").attr("src", initialImageSrc);
+  $(document).on("click", ".image-preview img", function () {
+    var imageSrc = $(this).attr("src");
 
-  // pdf文件
-  const pdfListContainer = $(".uploaded-file");
-  pdfListContainer.empty();
-  if (data.pdfFiles.length === 0) {
-    pdfListContainer.append('<li class="no-file">無檔案上傳</li>');
-  } else {
-    data.pdfFiles.forEach(function (pdfFile) {
-      const pdfElement = `<li><a href="${pdfFile.url}" target="_blank" download>${pdfFile.name} <i class="fa-solid fa-external-link-alt"></i></a></li>`;
-      pdfListContainer.append(pdfElement);
-    });
-    $(".upload-wrapper div").append(
-      '<p class="note">＊ 點擊檔案將自動下載</p>'
-    );
-  }
+    var modalContent = `<img src="${imageSrc}" alt="Image Preview" class="img-fluid" />`;
+    $("#filePreviewBody").html(modalContent);
+
+    $("#filePreviewModal").modal("show");
+  });
+  $("#goBack").on("click", function (event) {
+    event.preventDefault();
+    //alert("返回上一頁");
+    window.location.href = "records_maintenance.html";
+  });
 }
--- Zhixun/js/records_maintenance.js
+++ Zhixun/js/records_maintenance.js
@@ -1,146 +1,128 @@
 $(document).ready(function () {
-  let data = callAPI();
-
-  $(".district").text(data.district);
-  $(".building").text(data.building);
-
-  // 建立table
-  const tableBody = $("#records-table tbody");
-  tableBody.empty();
-  data.records.forEach(function (record) {
-    const row = `<tr data-link="${record.link}">
-                        <td>${record.title}</td>
-                        <td>${record.uploader}</td>
-                        <td>${record.uploadDate}</td>
-                     </tr>`;
-    tableBody.append(row);
+  const iconText = "機電"; // 設置預設值
+  const recordtext = "維護及施工紀錄查詢";
+  if (typeof BreadcrumbManager !== "undefined") {
+    console.log("1");
+    BreadcrumbManager.updateBreadcrumb(iconText, recordtext);
+  }
+  $(".recordtext").on("click", function () {
+    window.location.href = "records_maintenance.html";
   });
+  callAPI()
+    .then((data) => {
+      // console.log(data);
+      if (!Array.isArray(data)) {
+        console.error("API 返回的數據不是數組:", data);
+        return;
+      }
 
-  const table = $("#records-table").DataTable({
-    paging: true,
-    searching: true,
-    ordering: true,
-    pageLength: 5,
-    language: {
-      sProcessing: "處理中...",
-      sLengthMenu: "顯示 _MENU_ 項結果",
-      sZeroRecords: "沒有匹配結果",
-      sInfo: "顯示第 _START_ 至 _END_ 項結果，共 _TOTAL_ 項",
-      sInfoEmpty: "顯示第 0 至 0 項結果，共 0 項",
-      sInfoFiltered: "(從 _MAX_ 項結果過濾)",
-      sInfoPostFix: "",
-      sSearch: "搜索:",
-      sUrl: "",
-      paginate: {
-        previous: "&lt;",
-        next: "&gt;",
-      },
-    },
-  });
+      // 建立table
+      const tableBody = $("#records-table tbody");
+      tableBody.empty();
+      data.forEach(function (record) {
+        // console.log(record);
+        var date = record.date.substring(0, 10);
+        const row = `<tr data-link="${record.main_id}">
+                            <td>${record.main_class}</td>
+                            <td>${record.user}</td>
+                            <td>${date}</td>
+                         </tr>`;
+        tableBody.append(row);
+      });
 
-  $("#records-table tbody").on("click", "tr", function () {
-    const link = $(this).data("link");
-    if (link) {
-      window.location.href = link;
-    }
-  });
+      const table = $("#records-table").DataTable({
+        paging: true,
+        searching: true,
+        ordering: true,
+        pageLength: 5,
+        order: [[2, "desc"]],
+        language: {
+          sProcessing: "處理中...",
+          sLengthMenu: "顯示 _MENU_ 項結果",
+          sZeroRecords: "沒有匹配結果",
+          sInfo: "顯示第 _START_ 至 _END_ 項結果，共 _TOTAL_ 項",
+          sInfoEmpty: "顯示第 0 至 0 項結果，共 0 項",
+          sInfoFiltered: "(從 _MAX_ 項結果過濾)",
+          sInfoPostFix: "",
+          sSearch: "搜索:",
+          sUrl: "",
+          paginate: {
+            previous: "&lt;",
+            next: "&gt;",
+          },
+        },
+      });
 
-  $("#searchButton").on("click", function () {
-    const startDate = new Date($("#startDate").val());
-    const endDate = new Date($("#endDate").val());
-    const category = $("#category").val();
+      $("#records-table tbody").on("click", "tr", function () {
+        const main_id = $(this).data("link");
+        if (main_id) {
+          localStorage.setItem("mainid", main_id);
+          window.location.href = "preview_maintenance.html";
+        }
+      });
+      $("#searchButton").on("click", function () {
+        const startDate = new Date($("#startDate").val());
+        const endDate = new Date($("#endDate").val());
+        const category = $("#category").val();
+        // console.log(startDate, endDate);
+        // console.log("Settings", settings, data);
+        $.fn.dataTable.ext.search.push(function (settings, data) {
+          const date = new Date(data[2]);
+          const title = data[0];
+          const dateInRange = date >= startDate && date <= endDate;
+          const categoryMatch = !category || title.includes(category);
+          return dateInRange && categoryMatch;
+        });
 
-    $.fn.dataTable.ext.search.push(function (settings, data) {
-      const date = new Date(data[2]);
-      const title = data[0];
-      const dateInRange = date >= startDate && date <= endDate;
-      const categoryMatch = !category || title.includes(category);
-      return dateInRange && categoryMatch;
+        table.draw();
+        $.fn.dataTable.ext.search.pop();
+      });
+
+      // 時間日期設定
+      const startDatePicker = flatpickr("#startDate", {
+        enableTime: false,
+        dateFormat: "Y-m-d",
+        locale: "zh",
+        maxDate: "today",
+        time_24hr: false,
+        disableMobile: true,
+        allowInput: false,
+      });
+
+      const endDatePicker = flatpickr("#endDate", {
+        enableTime: false,
+        dateFormat: "Y-m-d",
+        time_24hr: false,
+        locale: "zh",
+        maxDate: "today",
+        disableMobile: true,
+        allowInput: false,
+      });
+
+      $(".input-wrapper i.fa-regular.fa-calendar").on("click", function () {
+        const inputId = $(this).siblings("input").attr("id");
+        if (inputId === "startDate") {
+          startDatePicker.open();
+        } else if (inputId === "endDate") {
+          endDatePicker.open();
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("API 調用失敗:", error);
     });
-    table.draw();
-    $.fn.dataTable.ext.search.pop();
-  });
-
-  // 時間日期設定
-  const startDatePicker = flatpickr("#startDate", {
-    enableTime: false,
-    dateFormat: "Y-m-d",
-    locale: "zh",
-    maxDate: "today",
-    time_24hr: false,
-    disableMobile: true,
-    allowInput: false,
-  });
-
-  const endDatePicker = flatpickr("#endDate", {
-    enableTime: false,
-    dateFormat: "Y-m-d",
-    time_24hr: false,
-    locale: "zh",
-    maxDate: "today",
-    disableMobile: true,
-    allowInput: false,
-  });
-
-  $(".input-wrapper i.fa-regular.fa-calendar").on("click", function () {
-    const inputId = $(this).siblings("input").attr("id");
-    if (inputId === "startDate") {
-      startDatePicker.open();
-    } else if (inputId === "endDate") {
-      endDatePicker.open();
-    }
-  });
 });
 
-function callAPI() {
-  const data = {
-    district: "鳳山區",
-    building: "建築A",
-    records: [
-      {
-        title: "施工",
-        uploader: "王小明",
-        uploadDate: "2024-09-21",
-        link: "preview_maintenance.html",
-      },
-      {
-        title: "保養",
-        uploader: "王中明",
-        uploadDate: "2024-09-22",
-        link: "preview_maintenance.html",
-      },
-      {
-        title: "施工",
-        uploader: "王大明",
-        uploadDate: "2024-09-23",
-        link: "preview_maintenance.html",
-      },
-      {
-        title: "施工",
-        uploader: "小安娜",
-        uploadDate: "2024-09-26",
-        link: "preview_maintenance.html",
-      },
-      {
-        title: "保養",
-        uploader: "大安娜",
-        uploadDate: "2024-09-27",
-        link: "preview_maintenance.html",
-      },
-      {
-        title: "施工",
-        uploader: "大安娜",
-        uploadDate: "2024-09-27",
-        link: "preview_maintenance.html",
-      },
-      {
-        title: "施工",
-        uploader: "大安娜",
-        uploadDate: "2024-09-27",
-        link: "preview_maintenance.html",
-      },
-    ],
-  };
-
-  return data;
+async function callAPI() {
+  try {
+    const data = await $.ajax({
+      url: `${window.API_CONFIG.baseUrl}/recordmaintenance`,
+      type: "GET",
+    });
+    console.log("Response data:", data);
+    return data.data;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error;
+  }
 }
