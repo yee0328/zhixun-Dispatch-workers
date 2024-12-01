1b0b0ccd33359e1f65fbc81760d8e957f231268d
yee0328 <ppp91032862@gmail.com>
2024-12-01 20:10:23 +0800

    "fix: 修改時間格式"

--- backend/models/plugins/system.js
+++ backend/models/plugins/system.js
@@ -54,10 +54,13 @@ function getTodayDate() {
 
   if (month < 10) {
     month = `0${month}`;
+  } else {
+    month = `${month}`;
   }
-
   if (day < 10) {
     day = `0${day}`;
+  } else {
+    day = `${day}`;
   }
 
   return year + month + day;
