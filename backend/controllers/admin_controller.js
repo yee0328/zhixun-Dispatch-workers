8eaf52c2bf9bb9da5a8de507cec70ac3adc090b4
yee0328 <ppp91032862@gmail.com>
2024-12-01 20:02:13 +0800

    "refactor: 將result更改成status"

--- backend/controllers/admin_controller.js
+++ backend/controllers/admin_controller.js
@@ -19,23 +19,23 @@ const login = async (req, res) => {
     if (account[0].status === 1) {
       res.status(httpStatus.OK).send({
         message: "登入成功",
-        result: "success",
+        status: "success",
       });
     } else {
       res.status(httpStatus.OK).send({
         message: "此帳號已停用",
-        result: "error",
+        status: "error",
       });
     }
   } else if (account === false) {
     res.status(httpStatus.OK).send({
       message: "登入失敗",
-      result: "error",
+      status: "error",
     });
   } else {
     res.status(httpStatus.OK).send({
       message: "登入失敗",
-      result: "error",
+      status: "error",
     });
   }
 };
