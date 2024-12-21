38c57815dbb92b6a305d7728ab83e2eed42bdeae
yee0328 <ppp91032862@gmail.com>
2024-12-21 20:22:24 +0800

    chore: 原先是將iconText寫死，後來因為開放其他功能，所以將其改為變數去更改文字

--- Zhixun/js/view.js
+++ Zhixun/js/view.js
@@ -24,34 +24,17 @@ if (orgdata && Array.isArray(orgdata) && orgdata.length > 0) {
   localStorage.removeItem("orgdata");
 }
 $(document).ready(function () {
-  const iconText = "機電"; // 設置預設值
+  const iconText = localStorage.getItem("uploadType");
   if (typeof BreadcrumbManager !== "undefined") {
-    console.log("1");
     BreadcrumbManager.updateBreadcrumb(iconText);
   }
   $(".upload").on("click", function () {
     var uploadType = $(this).find(".fa-solid").attr("class").split(" ").at(-1);
-    var uploadtext = $(this).text();
-
-    localStorage.setItem("uploadtext", uploadtext);
     window.location.href = `upload_${uploadType}.html`;
   });
-  if (!localStorage.getItem("orgdata")) {
-    console.log("beforeunload event was triggered");
-  } else {
-    console.log("fail");
-  }
 
   $(".record").on("click", function () {
     var recordType = $(this).find(".fa-solid").attr("class").split(" ").at(-1);
-    // var recordtext = $(this).text();
-    // console.log(recordtext);
-    // localStorage.setItem("recordtext", recordtext);
     window.location.href = `records_${recordType}.html`;
-    // if (recordType === "invoice") {
-    //   window.location.href = `records_invoice.html`;
-    // } else {
-    //   window.location.href = `records_rec.html`;
-    // }
   });
 });
