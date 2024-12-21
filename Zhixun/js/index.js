bbee1b933cc47fa8cfb14cf4ca3b41e1f4674fc8
yee0328 <ppp91032862@gmail.com>
2024-12-21 19:44:16 +0800

    chore: 修改登入驗證

--- Zhixun/js/index.js
+++ Zhixun/js/index.js
@@ -1,13 +1,18 @@
-var auth = JSON.parse(localStorage.getItem("auth"));
+var auth = localStorage.getItem("auth");
+if (auth) {
+  auth = JSON.parse(auth);
+} else {
+  auth = null;
+}
 if (
+  !auth ||
   auth.status !== "success" ||
   auth.date !==
     new Date().getFullYear() +
       "-" +
       (new Date().getMonth() + 1) +
       "-" +
-      new Date().getDate() ||
-  auth.status == null
+      new Date().getDate()
 ) {
   localStorage.removeItem("auth");
   window.location.href = "login.html";
