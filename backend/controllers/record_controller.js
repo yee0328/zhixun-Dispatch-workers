655c6054fad0cb5d893c96750a70fd0371fd094c
yee0328 <ppp91032862@gmail.com>
2024-12-23 21:59:12 +0800

    chore: 將debug的東西註解

--- backend/controllers/record_controller.js
+++ backend/controllers/record_controller.js
@@ -20,7 +20,7 @@ const recordreceipt = async (req, res) => {
 };
 const recordassessment = async (req, res) => {
   const uploadType = req.query.type;
-  console.log(uploadType);
+  // console.log(uploadType);
   const result = await recordService.recordassessment(uploadType);
   if (result.length > 0) {
     res.status(httpStatus.OK).send({
@@ -38,7 +38,7 @@ const recordassessment = async (req, res) => {
 const receiptDetail = async (req, res) => {
   const rec_id = req.query.rec_id;
   const result = await recordService.receiptDetail(rec_id);
-  console.log(result);
+  // console.log(result);
   if (result.length > 0) {
     res.status(httpStatus.OK).send({
       message: "查詢成功",
